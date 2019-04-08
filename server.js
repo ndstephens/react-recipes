09cd92d74027cebb27260dbc2f78e93796a1a3b0
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-08 18:13:36 -0400

    apollo server and middleware

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
 
 //? IMPORT MODELS / SCHEMAS
 const User = require('./models/User')
 const Recipe = require('./models/Recipe')
 
+//? IMPORT GRAPHQL TYPEDEFS AND RESOLVERS
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
+
+//
+
 //* INIT APP
 const app = express()
 const PORT = process.env.PORT || 4444
+
+//* CREATE GRAPHQL SCHEMA
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+
+//* MIDDLEWARE
+// Create GraphiQL application
+app.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }))
+// Connect schemas with GraphQL
+app.use(
+  '/graphql',
+  graphqlExpress({
+    schema,
+    context: { User, Recipe },
+  })
+)
 
 //* CONNECT TO DB
 mongoose
