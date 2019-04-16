5f7ea8c244c7cbb69db0aab5cdd1051a4cb6094f
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-16 11:17:41 -0400

    mutation

--- resolvers.js
+++ resolvers.js
@@ -2,4 +2,21 @@ exports.resolvers = {
   Query: {
     recipes: () => {},
   },
+  Mutation: {
+    addRecipe: async (
+      root,
+      { data: { name, description, category, instructions, username } },
+      { Recipe }
+    ) => {
+      const newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username,
+      }).save()
+
+      return newRecipe
+    },
+  },
 }
--- schema.js
+++ schema.js
@@ -1,26 +1,44 @@
 const { gql } = require('apollo-server-express')
 
 exports.typeDefs = gql`
+  #//? -------   QUERIES   --------
   type Query {
     recipes: [Recipe!]!
     users: [User!]!
   }
 
+  #//? -------   MUTATIONS   --------
+  type Mutation {
+    addRecipe(data: AddRecipeInput!): Recipe!
+  }
+
+  #//? -------   CUSTOM OBJECT TYPES   --------
   type Recipe {
+    _id: ID!
     name: String!
-    category: String!
     description: String!
+    category: String!
     instructions: String!
-    createdAt: String
-    likes: Int
+    createdAt: String!
+    likes: Int!
     username: String
   }
 
   type User {
+    _id: ID!
     username: String!
     password: String!
     email: String!
     joinDate: String
     favorites: [Recipe]
+  }
+
+  #//? -------   INPUT TYPES   --------
+  input AddRecipeInput {
+    name: String!
+    description: String!
+    category: String!
+    instructions: String!
+    username: String
   }
 `
