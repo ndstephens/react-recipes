b3de55d01459b92b09c8b1a3ab5bd01b2ce6f731
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-16 15:11:46 -0400

    form ui

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 
 class SignUp extends Component {
   render() {
-    return <div>SignUp</div>
+    return (
+      <div className="App">
+        <h2 className="App">SignUp</h2>
+        <form className="form">
+          <input type="text" name="username" placeholder="Username" />
+          <input type="email" name="email" placeholder="Email address" />
+          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirm Password"
+          />
+
+          <button type="submit" className="button-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    )
   }
 }
 
