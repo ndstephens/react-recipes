6f2990deb02e685d12d2756add24fa882b8325be
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-16 15:22:57 -0400

    make form controlled by state

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react'
 
 class SignUp extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
+    const { username, email, password, passwordConfirmation } = this.state
+
     return (
       <div className="App">
         <h2 className="App">SignUp</h2>
         <form className="form">
-          <input type="text" name="username" placeholder="Username" />
-          <input type="email" name="email" placeholder="Email address" />
-          <input type="password" name="password" placeholder="Password" />
           <input
+            autoFocus
+            value={username}
+            onChange={this.handleInputChange}
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
+          <input
+            value={email}
+            onChange={this.handleInputChange}
+            type="email"
+            name="email"
+            placeholder="Email address"
+          />
+          <input
+            value={password}
+            onChange={this.handleInputChange}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            value={passwordConfirmation}
+            onChange={this.handleInputChange}
             type="password"
             name="passwordConfirmation"
             placeholder="Confirm Password"
