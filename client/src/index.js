6bd20819b396076531807f88fdf2c94d0ba2f254
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-16 15:08:19 -0400

    routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
 import './index.css'
 import App from './components/App'
+import SignUp from './components/Auth/SignUp'
+import SignIn from './components/Auth/SignIn'
 
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
@@ -10,9 +19,20 @@ const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
 })
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+)
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById('root')
 )
