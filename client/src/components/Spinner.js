9b301edc8f9a2ce2cbe2748160f47653cbcdb813
Nathaniel Stephens <ndstephens@gmail.com>
2019-06-03 15:28:22 -0400

    update spinner color

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -3,7 +3,7 @@ import { PulseLoader } from 'react-spinners'
 
 const Spinner = props => (
   <div className="spinner">
-    <PulseLoader color={'#1eaedb'} size={50} margin={'3px'} />
+    <PulseLoader color={'#a5d6a7'} size={50} margin={'3px'} />
   </div>
 )
 
