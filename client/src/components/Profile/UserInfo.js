6b4d67a824dbc8b866b94cba877f8e5d3d2d2f63
Nathaniel Stephens <ndstephens@gmail.com>
2019-06-05 12:07:48 -0400

    update UserInfo

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -9,8 +9,8 @@ const formatDate = date => {
 }
 
 const UserInfo = ({ session }) => (
-  <div>
-    <h3>User Info</h3>
+  <div className="UserInfo">
+    <h2 className="orange-text text-accent-2">User Profile</h2>
     <p>Username: {session.currentUser.username}</p>
     <p>Email: {session.currentUser.email}</p>
     <p>Join Date: {formatDate(parseInt(session.currentUser.createdAt))}</p>
@@ -19,7 +19,7 @@ const UserInfo = ({ session }) => (
       {session.currentUser.favorites.map(favorite => (
         <li key={favorite._id}>
           <Link to={`/recipe/${favorite._id}`}>
-            <p>{favorite.name}</p>
+            <p className="orange-text text-accent-2">{favorite.name}</p>
           </Link>
         </li>
       ))}
